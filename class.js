a22fa1e68c0348f4ae0ea92f081926b633956bdc
hikoand <71857799+hikoand@users.noreply.github.com>
2022-04-20 19:09:43 +0900

    [U]  콜백함수를 이용한 제어

--- class.js
+++ class.js
@@ -5,15 +5,18 @@
 // Counter는 object를 만드는 순간 0 으로 초기화된다.
 
 class Counter {
-  constructor() {
+  constructor(runEveryFiveTimes) {
     this.counter = 0;
+    this.callback = runEveryFiveTimes;
   }
 
-  increase() {
+  increase(run5Times) {
     this.counter++;
     console.log(this.counter);
     if (this.counter % 5 === 0) {
-      console.log('5가 증가했습니다.');
+      // console.log('5가 증가했습니다.');
+      // run5Times(this.counter);
+      this.callback(this.counter);
     }
   }
 }
@@ -24,7 +27,27 @@ class Counter {
 
 // increase() 를 호출
 // class 안에 있는 counter 라는 데이터를 하나씩 증가시킴
-const coolCounter = new Counter();
+
+function printSomething(num) {
+  console.log(`yo! ${num}`);
+}
+function alertSomething(num) {
+  alert(`yo! ${num}`);
+}
+
+const coolCounter = new Counter(printSomething);
+
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+// coolCounter.increase(printSomething);
+
 coolCounter.increase();
 coolCounter.increase();
 coolCounter.increase();
@@ -33,3 +56,12 @@ coolCounter.increase();
 
 // counter 가 숫자 5가 될 때 마다 알고 싶다면?
 // 1. increase 자체에 조건문을 건다. but coolCounter 내부에서 제어할 수 없음
+// but coolCounter 사용자가 제어할 수 없음 (yo 말고 다른걸 보여주고싶다면?)
+
+// 2. 콜백함수로 전달
+// 원하는 기능 제어가능 (function printSomething(num), function alertSomething(num))
+// but 매번 (printSomething); 을 전달 해야햐서 불편함
+
+//3. cunstructor에서 콜백함수를 받음
+// runEveryFiveTimes 인자를 callback 이라는 변수에 할당
+// 생성자에 원하는 콜백함수를 전달 (printSomething)
